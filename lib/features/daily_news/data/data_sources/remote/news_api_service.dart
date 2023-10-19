b46ae977b246d88e840bc6500ac8d4a1bc1d3430
clufy <clufy220@gmail.com>
2023-10-20 03:26:58 +0500

    changes

--- lib/features/daily_news/data/data_sources/remote/news_api_service.dart
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -1,18 +1,19 @@
-import 'package:dio/dio.dart';
+
 import 'package:news_app/Core/constants/constants.dart';
 import 'package:news_app/features/daily_news/data/models/article.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
 part 'news_api_service.g.dart';
 
-@RestApi(baseUrl: newsAPIBaseUrl)
+@RestApi(baseUrl:newsAPIBaseUrl)
 abstract class NewsApiService {
   factory NewsApiService(Dio dio) = _NewsApiService;
 
-  @GET("/top-headlines")
+  @GET('/top-headlines')
   Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
     @Query("apiKey") String ? apiKey,
     @Query("country") String ? country,
     @Query("category") String ? category,
   });
 }
-
